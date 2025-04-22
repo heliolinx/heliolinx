700d6af7494b647720aa8d9652bd688fc05093a1
Mario Juric <mjuric@astro.washington.edu>
2025-04-21 17:15:34 -0700

    WIP

--- setup.py
+++ setup.py
@@ -8,11 +8,21 @@ import pybind11
 import numpy
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
+# select OpenMP build flags depending on the architecture
+import sys
+if sys.platform == 'darwin':
+    extra_link = ['-lomp']
+    extra_compile = ['-std=c++17', '-fopenmp']
+else:
+    extra_link = ['-lgomp']
+    extra_compile = []
+
 # Define the extension module
 heliolinx = Pybind11Extension(
     'heliolinx.heliolinx',
     sources=['src/heliolinx.cpp', 'src/solarsyst_dyn_geo01.cpp'],
-    extra_link_args=['-lgomp'],
+    extra_compile_args=extra_compile,
+    extra_link_args=extra_link,
     include_dirs=[
         pybind11.get_include(),  # Pybind11 include directory
         numpy.get_include()      # Numpy include directory
